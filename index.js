acd8c03a2e08bbc6fb16540def3053751327219f
Bryan Esteban Torres Cruz <Bryan.Esteban.Torres@ibm.com>
2024-06-28 13:52:34 -0500

    MasTests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
-import errorController from './controllers/errorController.js'
+import errorController from './errorController.js'
 
 const app = express()
 
